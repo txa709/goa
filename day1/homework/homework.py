a0dc81ccf266ba5729430ccd4c6544e52d49b25d
txa709 <vazhamgeladze7@gmail.com>
2024-06-13 14:14:34 +0400

    Add files via upload

---
+++ day1/homework/homework.py
@@ -0,0 +1,19 @@
+# ვპრინტავთ სახელს და გვარს
+
+
+# ვქმნით ცვადებს
+
+
+# lastneme = "gocadze"
+ 
+# age = "10"
+
+name ="nia"
+
+print("my names is",name)
+
+lastname = "gocadze"
+print("my lastname is",lastname)
+ 
+age = "10"
+print("my age is",age)
